22c67473e6d0f2fc73d20b711a1f6748fa2b1f3c
paulo-leitao <paulovictor.engca@gmail.com>
2022-05-06 12:37:32 -0300

    implementação da inserção de nova movimentação via método POST

--- src/app/components/movimentacao-new/movimentacao-new.component.ts
+++ src/app/components/movimentacao-new/movimentacao-new.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CorrentistaService } from 'src/app/services/correntista.service';
+import { MovimentacaoService } from 'src/app/services/movimentacao.service';
 
 @Component({
   selector: 'app-movimentacao-new',
@@ -6,10 +8,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movimentacao-new.component.css']
 })
 export class MovimentacaoNewComponent implements OnInit {
+  
+  //Irá receber os dados de correntista da nossa requisição
+  correntista: any;
+  correntistaSelec: any;
 
-  constructor() { }
+  descricao: any;
+  valor: any;
+  dataHora: any;
+  tipo: any;
 
+  constructor(
+    private movimentacaoService: MovimentacaoService,
+    private correntistaService: CorrentistaService
+  ) { }
+  
+  // Ao iniciar, irá chamar a requisição http do tipo get através do nosso service criado
   ngOnInit(): void {
+    this.exibirCorrentista();
+  }
+
+  exibirCorrentista(): void{
+    this.correntistaService.list()
+      .subscribe(
+        data => {
+          this.correntista = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        }
+      )
+  }
+
+  save(): void {
+    console.log(this.correntistaSelec);
+    const movimentacao = {
+      valor: this.valor,
+      descricao: this.descricao,
+      tipo: this.tipo,
+      dataHora: this.dataHora,
+      idConta: this.correntistaSelec.id
+    };
+    console.log(movimentacao);
+    this.movimentacaoService.create(movimentacao)
+      .subscribe(
+        response => {
+          console.log(response);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    
   }
 
 }
--- src/app/services/correntista.service.ts
+++ src/app/services/correntista.service.ts
@@ -2,25 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-//Endereço na url da nossa API local ou remota (heroku)
 // const baseUrl = 'http://localhost:8080';
 const baseUrl = 'https://pleitao-dio-bankline-api.herokuapp.com';
 
 @Injectable({
   providedIn: 'root'
 })
-export class MovimentacaoService {
+export class CorrentistaService {
 
   constructor( private http: HttpClient) { }
 
-
-  // Método de requisição get via api 
   list(): Observable<any> {
-    return this.http.get(`${baseUrl}/movimentacao`);
+    return this.http.get(`${baseUrl}/correntista`);
   }
-  
-  // Método de inclusão post via api
-  create(movimentacao:any): Observable<any> {
-    return this.http.post(`${baseUrl}/movimentacao`, movimentacao);
-  }
+
 }
